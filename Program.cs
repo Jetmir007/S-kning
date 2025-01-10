b194c2320ac05b6741ed6868844750051e276d11
Jetmir007 <jeku07001@utb.vaxjo.se>
2025-01-10 13:37:10 +0100

    Binär Sökning

--- Program.cs
+++ Program.cs
@@ -7,21 +7,34 @@ static int BinSearch(List<Person> pl, int first, int last, int key){
             first = mid +1;
         }
         else if( pl[mid].Personnr > key){
-            
+            last = mid -1;
+        }
+        else{
+            return mid;
         }
     }
+    return -1;
 }
 
-List<Person> personlista = new List<Person>();
-personlista.Add(new Person(1, "Darth Vader"));
-personlista.Add(new Person(2, "Luke Skywalker"));
-personlista.Add(new Person(3, "Leia Organa"));
-personlista.Add(new Person(4, "Han Solo"));
-personlista.Add(new Person(5, "R2D2"));
-personlista.Add(new Person(6, "C3PO"));
-personlista.Add(new Person(7, "Lando Calrissian"));
-personlista.Add(new Person(8, "Obi-Wan"));
+List<Person> personlista =
+[
+    new Person(1, "Darth Vader"),
+    new Person(2, "Luke Skywalker"),
+    new Person(3, "Leia Organa"),
+    new Person(4, "Han Solo"),
+    new Person(5, "R2D2"),
+    new Person(6, "C3PO"),
+    new Person(7, "Lando Calrissian"),
+    new Person(8, "Obi-Wan"),
+    new Person(9, "Qui-Gon-Jin"),
+];
 
 Console.WriteLine("Ange personnummret");
 int nyckel = Convert.ToInt32(Console.ReadLine());
-int index = BinSearch(personlista, nyckel);
+int index = BinSearch(personlista,0, 8, nyckel);
+if(index != -1){
+    Console.WriteLine(personlista[index].Namn);
+}
+else{
+    Console.WriteLine("Personnumret finns inte med i listan");
+}
--- obj/Debug/net8.0/S-kning.AssemblyInfo.cs
+++ obj/Debug/net8.0/S-kning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S-kning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989412e8c9198c7c4527c6764762e491a3bca0b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a9db51851ef3c523b802199cb8e9b014df0373")]
 [assembly: System.Reflection.AssemblyProductAttribute("S-kning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S-kning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
